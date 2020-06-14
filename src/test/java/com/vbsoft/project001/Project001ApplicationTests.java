3f9c0247ac57f61493d4c11216d467cb7e9637d9
lucasbaldusca <lucasbaldusca@hotmail.com>
2020-06-14 00:12:11 -0300

    Project created

---
+++ src/test/java/com/vbsoft/project001/Project001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vbsoft.project001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
