22638f1277b8f9391660ae3a42086230c06bc6c4
lucasbaldusca <lucasbaldusca@hotmail.com>
2020-08-11 23:34:16 -0300

    Order, Instant, ISO 8601 - PART1

--- src/main/java/com/vbsoft/project001/entities/User.java
+++ src/main/java/com/vbsoft/project001/entities/User.java
@@ -1,103 +1,107 @@
- package com.vbsoft.project001.entities;
+package com.vbsoft.project001.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-		@Id
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
-		private Long id;
-		
-		
-		private String name;
-		private String email;
-		private String phone;
-		private String password;
-		
-		public User () {
-		}
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-		public User(Long id, String name, String email, String phone, String password) {
-			super();
-			this.id = id;
-			this.name = name;
-			this.email = email;
-			this.phone = phone;
-			this.password = password;
-		}
+	private String name;
+	private String email;
+	private String phone;
+	private String password;
 
-		public Long getId() {
-			return id;
-		}
+	@OneToMany(mappedBy ="client")
+	private List<Order> orders = new ArrayList<>();
 
-		public void setId(Long id) {
-			this.id = id;
-		}
+	public User() {
+	}
 
-		public String getName() {
-			return name;
-		}
+	public User(Long id, String name, String email, String phone, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
 
-		public void setName(String name) {
-			this.name = name;
-		}
+	public Long getId() {
+		return id;
+	}
 
-		public String getEmail() {
-			return email;
-		}
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-		public void setEmail(String email) {
-			this.email = email;
-		}
+	public String getName() {
+		return name;
+	}
 
-		public String getPhone() {
-			return phone;
-		}
+	public void setName(String name) {
+		this.name = name;
+	}
 
-		public void setPhone(String phone) {
-			this.phone = phone;
-		}
+	public String getEmail() {
+		return email;
+	}
 
-		public String getPassword() {
-			return password;
-		}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-		public void setPassword(String password) {
-			this.password = password;
-		}
+	public String getPhone() {
+		return phone;
+	}
 
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + ((id == null) ? 0 : id.hashCode());
-			return result;
-		}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			User other = (User) obj;
-			if (id == null) {
-				if (other.id != null)
-					return false;
-			} else if (!id.equals(other.id))
-				return false;
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
 			return true;
-		}
-		
-		
-	
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 }
